88175533b1c202415e444e1c8d0f9d59f8de948d
Corentin-Delvaque <corentin.delvaque@icloud.com>
2026-03-16 11:55:46 +0100

    Add theme functions, templates, and WooCommerce components
    - Created functions.php to register styles and scripts for the theme.
    - Added various HTML templates for WooCommerce functionalities including:
      - Checkout header
      - Coming soon social links
      - Product add-to-cart options for external, grouped, simple, and variable products
      - Mini cart structure
      - Archive, single product, and search results templates
      - Order confirmation and cart page templates
      - Footer pattern for consistent styling
    - Implemented Gutenberg block styles and settings for enhanced editor experience.

---
+++ patterns/footer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Title: footer
+ * Slug: /footer
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"align":"full","className":"wc-blocks-pattern-footer-large wc-blocks-footer-pattern","style":{"spacing":{"padding":{"top":"40px","right":"40px","bottom":"40px","left":"40px"},"blockGap":"40px"}}} -->
+<div class="wp-block-group alignfull wc-blocks-pattern-footer-large wc-blocks-footer-pattern" style="padding-top:40px;padding-right:40px;padding-bottom:40px;padding-left:40px"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":"32px","padding":{"right":"0px","left":"0px"}}}} -->
+<div class="wp-block-columns alignwide" style="padding-right:0px;padding-left:0px"><!-- wp:column {"width":"60%","style":{"spacing":{"padding":{"right":"50px"}}}} -->
+<div class="wp-block-column" style="padding-right:50px;flex-basis:60%"><!-- wp:group {"style":{"spacing":{"blockGap":"8px"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:site-logo /-->
+
+<!-- wp:heading {"level":5,"style":{"typography":{"textTransform":"none"},"spacing":{"margin":{"top":"40px"}}}} -->
+<h5 class="wp-block-heading" style="margin-top:40px;text-transform:none"><?php esc_html_e('Rejoindre la communauté', '');?></h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"bottom":"40px"}}}} -->
+<p style="margin-bottom:40px"><?php esc_html_e('Découvrir les nouveaux produits et les remises', '');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"size":"has-small-icon-size","className":"has-icon-color is-style-logos-only","style":{"spacing":{"blockGap":{"left":"16px"}}}} -->
+<ul class="wp-block-social-links has-small-icon-size has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"https://www.facebook.com","service":"facebook","className":"has-contrast-color"} /-->
+
+<!-- wp:social-link {"url":"https://www.x.com","service":"x","className":"has-contrast-color"} /-->
+
+<!-- wp:social-link {"url":"https://www.instagram.com","service":"instagram","className":"has-contrast-color"} /-->
+
+<!-- wp:social-link {"url":"https://www.twitch.com","service":"twitch","className":"has-contrast-color"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"20%","style":{"spacing":{"padding":{"top":"0px"}}}} -->
+<div class="wp-block-column" style="padding-top:0px;flex-basis:20%"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top","width":"20%","style":{"spacing":{"blockGap":"16px"}}} -->
+<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:20%"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top","width":"20%","style":{"spacing":{"blockGap":"16px"}}} -->
+<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:20%"><!-- wp:navigation {"overlayMenu":"never","style":{"spacing":{"blockGap":"10px"}},"layout":{"overlayMenu":"never","type":"flex","orientation":"vertical"}} /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"blockGap":"10px"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
+<div class="wp-block-group alignfull" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:site-title {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}}} /-->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">
+			Propulsé par <a href="https://wordpress.org" target="_blank" rel="noreferrer nofollow">WordPress</a> avec <a href="https://woocommerce.com" target="_blank" rel="noreferrer nofollow">WooCommerce</a>		</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
