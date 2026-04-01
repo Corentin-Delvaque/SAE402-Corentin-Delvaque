8026aef5ccd53941615fffd83652172d0ddea48a
Corentin Delvaque <corentin.delvaque@icloud.com>
2026-04-01 13:13:44 +0200

    Refactor code structure for improved readability and maintainability

--- patterns/footer.php
+++ patterns/footer.php
@@ -6,93 +6,53 @@
  */
 ?>
 <!-- wp:group {"align":"full","className":"wc-blocks-pattern-footer-large wc-blocks-footer-pattern","style":{"spacing":{"padding":{"right":"40px","left":"40px","top":"40px","bottom":"40px"},"blockGap":"96px","margin":{"top":"0","bottom":"0"}},"dimensions":{"minHeight":"0px"},"border":{"width":"0px","style":"none","radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"0px","bottomRight":"0px"}}},"backgroundColor":"custom-black"} -->
-<div class="wp-block-group alignfull wc-blocks-pattern-footer-large wc-blocks-footer-pattern has-custom-black-background-color has-background" style="border-style:none;border-width:0px;border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:0px;min-height:0px;margin-top:0;margin-bottom:0;padding-top:40px;padding-right:40px;padding-bottom:40px;padding-left:40px"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":"32px","padding":{"right":"0px","left":"0px"}}}} -->
-<div class="wp-block-columns alignwide" style="padding-right:0px;padding-left:0px"><!-- wp:column {"width":"60%","style":{"spacing":{"padding":{"right":"0px","top":"0","bottom":"0","left":"0"},"blockGap":"var:preset|spacing|40"}}} -->
-<div class="wp-block-column" style="padding-top:0;padding-right:0px;padding-bottom:0;padding-left:0;flex-basis:60%"><!-- wp:site-logo {"width":64,"shouldSyncIcon":true,"className":"is-style-default"} /-->
+<div class="wp-block-group alignfull wc-blocks-pattern-footer-large wc-blocks-footer-pattern has-custom-black-background-color has-background" style="border-style:none;border-width:0px;border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:0px;min-height:0px;margin-top:0;margin-bottom:0;padding-top:40px;padding-right:40px;padding-bottom:40px;padding-left:40px"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"60px","left":"100px"},"padding":{"right":"0px","left":"0px"}}}} -->
+<div class="wp-block-columns alignwide" style="padding-right:0px;padding-left:0px"><!-- wp:column {"width":"200%","style":{"spacing":{"padding":{"right":"0px","top":"0","bottom":"0","left":"0"},"blockGap":"var:preset|spacing|40"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-column" style="padding-top:0;padding-right:0px;padding-bottom:0;padding-left:0;flex-basis:200%"><!-- wp:template-part {"slug":"model-3-d","area":"uncategorized"} /-->
 
-<!-- wp:heading {"level":3} -->
-<h3 class="wp-block-heading"><?php esc_html_e('Rejoindre la communauté', '');?></h3>
+<!-- wp:heading {"level":3,"fontSize":"large"} -->
+<h3 class="wp-block-heading has-large-font-size"><?php esc_html_e('Rejoindre la communauté', '');?></h3>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph -->
-<p><?php esc_html_e('tfyguiop^$', '');?></p>
+<!-- wp:paragraph {"fontSize":"navigation"} -->
+<p class="has-navigation-font-size"><?php esc_html_e('tfyguiop^$', '');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"width":"20%","style":{"spacing":{"padding":{"top":"0px"}}}} -->
-<div class="wp-block-column" style="padding-top:0px;flex-basis:20%"><!-- wp:heading -->
-<h2 class="wp-block-heading"><?php esc_html_e('Accueil', '');?></h2>
+<!-- wp:column {"width":"100%","style":{"spacing":{"padding":{"top":"0px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-column" style="padding-top:0px;flex-basis:100%"><!-- wp:heading {"fontSize":"large"} -->
+<h2 class="wp-block-heading has-large-font-size"><?php esc_html_e('Accueil', '');?></h2>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph -->
-<p><?php esc_html_e('Accueil', '');?></p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p><?php esc_html_e('Se connecter', '');?></p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p><?php esc_html_e('Panier', '');?></p>
-<!-- /wp:paragraph --></div>
+<!-- wp:page-list {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-white"}}}},"backgroundColor":"custom-transparent","textColor":"custom-white","fontSize":"navigation"} /--></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"width":"20%","style":{"spacing":{"padding":{"top":"0px"}}}} -->
-<div class="wp-block-column" style="padding-top:0px;flex-basis:20%"><!-- wp:heading -->
-<h2 class="wp-block-heading"><?php esc_html_e('Produits', '');?></h2>
+<!-- wp:column {"width":"100%","style":{"spacing":{"padding":{"top":"0px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-column" style="padding-top:0px;flex-basis:100%"><!-- wp:heading {"fontSize":"large"} -->
+<h2 class="wp-block-heading has-large-font-size"><?php esc_html_e('Produits', '');?></h2>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph -->
-<p><?php esc_html_e('Armes', '');?></p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p><?php esc_html_e('Potions', '');?></p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p><?php esc_html_e('Bonus', '');?></p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p><?php esc_html_e('Médailles', '');?></p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p><?php esc_html_e('Clés', '');?></p>
-<!-- /wp:paragraph --></div>
+<!-- wp:categories {"taxonomy":"product_cat","showLabel":false,"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-white"}}}},"textColor":"custom-white","fontSize":"navigation"} /--></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"width":"20%","style":{"spacing":{"padding":{"top":"0px"}}}} -->
-<div class="wp-block-column" style="padding-top:0px;flex-basis:20%"><!-- wp:heading -->
-<h2 class="wp-block-heading"><?php esc_html_e('A Propos', '');?></h2>
+<!-- wp:column {"width":"100%","style":{"spacing":{"padding":{"top":"0px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-column" style="padding-top:0px;flex-basis:100%"><!-- wp:heading {"fontSize":"large"} -->
+<h2 class="wp-block-heading has-large-font-size"><?php esc_html_e('A Propos', '');?></h2>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph -->
-<p><?php esc_html_e('A propos de notre boutique', '');?></p>
+<!-- wp:paragraph {"fontSize":"navigation"} -->
+<p class="has-navigation-font-size"><?php esc_html_e('A propos de notre boutique', '');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
 
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"blockGap":"10px"}},"backgroundColor":"custom-transparent","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
-<div class="wp-block-group alignfull has-custom-transparent-background-color has-background" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center"><?php esc_html_e('Termes et confidentialité', '');?></p>
-<!-- /wp:paragraph -->
+<!-- wp:buttons {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="http://la-boutique-zelda.wp.local/privacy-policy/"><?php esc_html_e('Politique de confidentialité', '');?></a></div>
+<!-- /wp:button -->
 
-<!-- wp:separator -->
-<hr class="wp-block-separator has-alpha-channel-opacity"/>
-<!-- /wp:separator -->
-
-<!-- wp:paragraph -->
-<p><?php esc_html_e('Termes et conditions', '');?></p>
-<!-- /wp:paragraph -->
-
-<!-- wp:separator -->
-<hr class="wp-block-separator has-alpha-channel-opacity"/>
-<!-- /wp:separator -->
-
-<!-- wp:paragraph -->
-<p><?php esc_html_e('Préférences de cookies', '');?></p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:group --></div>
+<!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="http://la-boutique-zelda.wp.local/remboursements_retours/"><?php esc_html_e('Politique de remboursements', '');?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
 <!-- /wp:group -->
--- patterns/produit.php
+++ patterns/produit.php
@@ -6,11 +6,11 @@
  */
 ?>
 <!-- wp:woocommerce/featured-product {"contentAlign":"left","dimRatio":0,"focalPoint":{"x":1,"y":0.35},"imageFit":"cover","productId":382,"borderColor":"custom-or","backgroundColor":"custom-green","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}},"border":{"width":"2px"}}} -->
-<!-- wp:post-title {"textAlign":"left","isLink":true,"style":{"typography":{"fontSize":"3.9rem"},"spacing":{"margin":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|40"}}},"__woocommerceNamespace":"woocommerce/product-collection/product-title"} /-->
+<!-- wp:post-title {"textAlign":"left","isLink":true,"style":{"spacing":{"margin":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|40"}}},"fontSize":"x-large","__woocommerceNamespace":"woocommerce/product-collection/product-title"} /-->
 
 <!-- wp:columns {"style":{"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"0","bottom":"0"},"blockGap":{"top":"0"}}}} -->
 <div class="wp-block-columns" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0"><!-- wp:column {"width":"50%"} -->
-<div class="wp-block-column" style="flex-basis:50%"><!-- wp:woocommerce/product-summary {"showDescriptionIfEmpty":true,"summaryLength":200,"style":{"typography":{"textAlign":"left"},"spacing":{"margin":{"right":"0em","left":"0"}}}} -->
+<div class="wp-block-column" style="flex-basis:50%"><!-- wp:woocommerce/product-summary {"showDescriptionIfEmpty":true,"summaryLength":200,"fontSize":"medium","style":{"typography":{"textAlign":"left"},"spacing":{"margin":{"right":"0em","left":"0"}}}} -->
 <div class="is-loading"></div>
 <!-- /wp:woocommerce/product-summary --></div>
 <!-- /wp:column -->
@@ -20,12 +20,12 @@
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
 
-<!-- wp:woocommerce/product-price {"textAlign":"left","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|40","top":"var:preset|spacing|40"},"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}},"typography":{"fontSize":"1.5rem"}}} -->
+<!-- wp:woocommerce/product-price {"textAlign":"left","fontSize":"large","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|40","top":"var:preset|spacing|40"},"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}}} -->
 <div class="is-loading"></div>
 <!-- /wp:woocommerce/product-price -->
 
-<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left"}} -->
-<div class="wp-block-buttons"><!-- wp:button -->
+<!-- wp:buttons {"backgroundColor":"custom-transparent","layout":{"type":"flex","justifyContent":"left"}} -->
+<div class="wp-block-buttons has-custom-transparent-background-color has-background"><!-- wp:button -->
 <div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="http://la-boutique-zelda.wp.local/produit/epee-de-legende/"><?php esc_html_e('Acheter maintenant', '');?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons -->
@@ -35,8 +35,8 @@
 <h2 class="wp-block-heading has-text-align-center"><?php esc_html_e('CODE PROMO', '');?></h2>
 <!-- /wp:heading -->
 
-<!-- wp:woocommerce/coupon-code {"couponCode":"la boutique de terry","backgroundColor":"custom-turquoise","textColor":"custom-white","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"margin":{"top":"0","bottom":"0","left":"0","right":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|custom-white"}}}}} -->
-<div class="wp-block-woocommerce-coupon-code has-custom-white-color has-custom-turquoise-background-color has-text-color has-background has-link-color" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><strong>la boutique de terry</strong></div>
+<!-- wp:woocommerce/coupon-code {"couponCode":"la boutique de terry","backgroundColor":"custom-turquoise","textColor":"custom-white","fontSize":"medium","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"margin":{"top":"0","bottom":"0","left":"0","right":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|custom-white"}}}}} -->
+<div class="wp-block-woocommerce-coupon-code has-custom-white-color has-custom-turquoise-background-color has-text-color has-background has-link-color has-medium-font-size" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><strong>la boutique de terry</strong></div>
 <!-- /wp:woocommerce/coupon-code --></div>
 <!-- /wp:group -->
 <!-- /wp:woocommerce/featured-product -->
