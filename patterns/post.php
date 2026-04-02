60d4efa970a8b95872c9cb2acea7f1a5910ec7d2
Corentin Delvaque <corentin.delvaque@etu.unilim.fr>
2026-04-03 01:06:16 +0200

    Mise à jour des fichiers de modèle et ajout de nouveaux modèles 3D, refactorisation des parties de l'en-tête et du pied de page, et correction des liens dans le theme.json pour une meilleure intégration des ressources.

---
+++ patterns/post.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Title: Post
+ * Slug: post
+ * Categories: 
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|70","left":"var:preset|spacing|70","top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"}}},"backgroundColor":"custom-transparent","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-custom-transparent-background-color has-background" style="padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)"><!-- wp:heading {"fontSize":"x-large"} -->
+<h2 class="wp-block-heading has-x-large-font-size"><strong>Posts</strong></h2>
+<!-- /wp:heading -->
+
+<!-- wp:group {"style":{"border":{"radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"0px","bottomRight":"0px"},"width":"0px","style":"none"},"spacing":{"padding":{"right":"0","left":"0"}}},"backgroundColor":"custom-transparent","layout":{"type":"grid","columnCount":3,"minimumColumnWidth":null}} -->
+<div class="wp-block-group has-custom-transparent-background-color has-background" style="border-style:none;border-width:0px;border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:0px;padding-right:0;padding-left:0"><!-- wp:image {"id":218,"width":"840px","height":"auto","sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large is-resized"><img src="https://mmi.unilim.fr/~thierry18/laboutiquezelda/wp-content/uploads/2026/04/Instagram-Post-Mockup-693x1024.png" alt="" class="wp-image-218" style="width:840px;height:auto"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":217,"width":"840px","height":"auto","sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large is-resized"><img src="https://mmi.unilim.fr/~thierry18/laboutiquezelda/wp-content/uploads/2026/04/Instagram-Post-Mockup-2-693x1024.png" alt="" class="wp-image-217" style="width:840px;height:auto"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":219,"width":"840px","height":"auto","sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large is-resized"><img src="https://mmi.unilim.fr/~thierry18/laboutiquezelda/wp-content/uploads/2026/04/Instagram-Post-Mockup-3-693x1024.png" alt="" class="wp-image-219" style="width:840px;height:auto"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
